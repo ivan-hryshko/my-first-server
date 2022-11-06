b6d70cbecaaa77a66c9bba5368c2dcb9c1af6244
ivan.hryshko <ihryshko@datrics.ai>
2022-11-06 13:09:25 +0200

    create server at Application

--- index.js
+++ index.js
@@ -1,72 +1,16 @@
-const http = require('http')
-const EventEmitter = require('events')
+const Router = require('./framework/Router')
+const Application = require('./framework/Application')
 const PORT = process.env.PORT || 5000
 
-const emitter = new EventEmitter()
-class Router {
-  constructor() {
-    this.endpoints = {}
-  }
-
-  request(method = 'GET', path, handler) {
-    if (!this.endpoints[path]) {
-      this.endpoints[path] = {}
-    }
-
-    const endpoint = this.endpoints[path]
-
-    if (endpoint[method]) {
-      throw new Error (`[${method} at address ${path} is exist]`)
-    }
-
-    endpoint[method] = handler
-    emitter.on(`[${path}]:[${method}]`, (req, res) => {
-      handler(req, res)
-    })
-  }
-
-  get(path, handler) {
-    this.request('GET', path, handler)
-  }
-  post(path, handler) {
-    this.request('POST', path, handler)
-  }
-  put(path, handler) {
-    this.request('PUT', path, handler)
-  }
-  delete(path, handler) {
-    this.request('DELETE', path, handler)
-  }
-}
-
 const router = new Router()
+const app = new Application()
+
 router.get('/users', (req, res) => {
-  res.send('YOU SEND REQUEST TO /USERS')
+  res.end('YOU SEND REQUEST TO /USERS')
 })
 router.get('/posts', (req, res) => {
-  res.send('YOU SEND REQUEST TO /POSTS')
+  res.end('YOU SEND REQUEST TO /POSTS')
 })
 
-const server = http.createServer((req, res) => {
-  // res.writeHead(200, {
-  //   'Content-type': 'text/html; charset=utf-8 '
-  // })
-  res.writeHead(200, {
-    'Content-type': 'application/json;'
-  })
-  // res.end('І Ї працює!')
-  // res.end('<h1>Hello world</h1>')
- //
-  if (req.url === '/post') {
-    res.end('POSTS')
-  }
-  if (req.url === '/users') {
-    res.end('USERS')
-  }
-  res.end(JSON.stringify({
-    id: 1,
-    name: 'ivan'
-  }))
-})
-
-server.listen(PORT, () => console.log(`Server start on PORT: ${PORT}`))
+app.addRouter(router)
+app.listen(PORT, () => console.log(`Server start on PORT: ${PORT}`))
